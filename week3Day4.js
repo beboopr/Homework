d122f4deea56b78f2e7b284d8701397e1aed2b23
beboopr <linnets-router.09@icloud.com>
2022-10-21 11:49:36 -0400

    week3D4HW

--- week3Day4.js
+++ week3Day4.js
@@ -1,5 +1,21 @@
-function addingNum ( a, b ) {
-    if (addingNum => 10) {
-        console.log('true')
-    }
-}
+
+// function addingTwoNum(a, b) {
+//     if (a + b == 10) {
+//         console.log(true)
+//     }
+//     else if (a === 10) {
+//         console.log(true)
+//     }
+//     else if (b === 10) {
+//         console.log(true)
+//     }
+//     else console.log(false)
+// }
+
+// addingTwoNum(10,10)
+
+//-------------------------------------//
+//create a function that removes the first and last characters of a string
+const str = "Honey"
+const withoutFirstAndLast = str.slice(1, -1);
+console.log(withoutFirstAndLast);
