0382c7b9b79dbb4e1893c2339e5d3bd2242a15d9
beboopr <linnets-router.09@icloud.com>
2024-06-17 21:48:13 -0400

    practice

---
+++ practice1.py
@@ -0,0 +1,19 @@
+#building a network coffe shop..
+
+#print("Welcome to the coffee shop. Here's our menu: ")
+#print("1. Coffee")
+#print("2. Tea")
+#print("3. Water")
+
+#print("What would you like to order? ")
+
+#order = input()
+
+#print("You ordered: ", order)
+#print("is that correct?")
+#answer = input()
+
+#name = input("What is your name? ")
+
+#print("Thank you", name , "for your order. Your order will be ready shortly.")
+
