9dcf8deeae8dea38b1e0e3f878cbbe513e522f88
beboopr <linnets-router.09@icloud.com>
2024-06-18 22:37:03 -0400

    practice

--- practice1.py
+++ practice1.py
@@ -1,7 +1,22 @@
-#building a network coffe shop..
+#building a network coffe shop, were the costumer can order from a menu
+#and the program will give the total price and the time it will be ready
 
-print("Welcome to the coffee shop. Here's our menu: ")
-Menu = ("Coffee", "Tea", "Water", "Soda")
+print("Welcome to the coffee shop.\n")
+
+name = input("What is your name?\n")
+
+if name == "bob":
+    evil_status = input("Are you evil?\n")
+    if evil_status == "yes\n":
+        print("Sorry, your not Welocme here.")
+        exit()
+    else:
+        print("Ohh your one of the good ones.\n")
+else:
+    print("Welcome to the coffee shop. Here's our menu:", name)
+
+
+Menu = ("Coffee", "Tea", "Water", "Soda", "Water with Lemon")
 
 print("What would you like to order? ")
 
@@ -12,13 +27,24 @@ print("is that correct?")
 
 answer = input()
 
-name = input("What is your name? ")
-
 price = 8.99
 
 quantity = (input("How many would you like? "))
 
+if order == "Coffee":
+    price = 8.99
+elif order == "Tea":
+    price = 4.99
+elif order == "Water":
+    price = 2.99
+elif order == "Water with Lemon":
+    price = 3.99
+elif order == "Soda":
+    price = 3.99
+else:
+    print("Sorry, we don't have that.")
+
 total = price * int(quantity)
 
 print("Thank you", name, "for your", order, "order.")
-print("Your total is: $",total, "and your" ,int(quantity),order, "will be ready shortly.")
+print("Your total is: $",total, "and your" ,int(quantity),order,"will be ready shortly.")
