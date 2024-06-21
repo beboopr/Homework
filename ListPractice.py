2f9b280f7f10db9c709ee64d54d684692b9892bd
beboopr <linnets-router.09@icloud.com>
2024-06-21 00:15:56 -0400

    List methods

---
+++ ListPractice.py
@@ -0,0 +1,14 @@
+#Were going on a camping trip! We need to pack our bags
+#with the essentials.
+
+#Create a list of items that we need to pack for our trip.
+
+camp_stuff = ["tent", "sleeping bag", "water", "water", "rasberry-pi",
+            "clothes", "flashlight", "first aid kit", "map",
+            "portable fan"]
+
+camp_site = ["Lakewaters", 404, 89.2, 10, False]
+
+supplies = ["toilet paper"]
+
+print(supplies)
