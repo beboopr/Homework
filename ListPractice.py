e896a4260c08ab5161aed309d4d1e2ee9b40ba81
beboopr <linnets-router.09@icloud.com>
2024-06-21 00:26:10 -0400

    methos

--- ListPractice.py
+++ ListPractice.py
@@ -3,12 +3,14 @@
 
 #Create a list of items that we need to pack for our trip.
 
-camp_stuff = ["tent", "sleeping bag", "water", "water", "rasberry-pi",
+supplies = ["tent", "sleeping bag", "water", "water", "rasberry-pi",
             "clothes", "flashlight", "first aid kit", "map",
             "portable fan"]
 
 camp_site = ["Lakewaters", 404, 89.2, 10, False]
 
-supplies = ["toilet paper"]
+#supplies.append("toilet paper")
+supplies.extend(["toilet paper", "bug spray", "dominoes"])
+supplies.insert(0, "toilet paper")
 
 print(supplies)
