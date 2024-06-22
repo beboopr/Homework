f08d7cd2f3a6d2c27a201a8667f951b26af330f9
beboopr <linnets-router.09@icloud.com>
2024-06-22 02:00:49 -0400

    added ".pop"

--- py/ListPractice.py
+++ py/ListPractice.py
@@ -3,14 +3,15 @@
 
 #Create a list of items that we need to pack for our trip.
 
-supplies = ["tent", "sleeping bag", "water", "water", "rasberry-pi",
-            "clothes", "flashlight", "first aid kit", "map",
-            "portable fan"]
+supplies = ["tent", "sleeping bag", "water", "water", "rasberry-pi"]
 
 camp_site = ["Lakewaters", 404, 89.2, 10, False]
 
 #supplies.append("toilet paper")
-supplies.extend(["toilet paper", "bug spray", "dominoes"])
-supplies.insert(0, "toilet paper")
+supplies.extend(["rasberry-pi", "bug spray", "dominoes"])
+#supplies.insert(0, "rasberry-pi")
+#supplies.remove("tent")
+supplies.pop(0)
+
 
 print(supplies)
